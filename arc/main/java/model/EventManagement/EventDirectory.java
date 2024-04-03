fc122098754a9f508895583fc0f7dfac7378413e
Yutong66 <145811791+Yutong66@users.noreply.github.com>
2024-04-03 16:19:08 -0700

    111

--- arc/main/java/model/EventManagement/EventDirectory.java
+++ arc/main/java/model/EventManagement/EventDirectory.java
@@ -2,10 +2,26 @@ package arc.main.java.model.EventManagement;
 
 import java.util.ArrayList;
 
+import arc.main.java.model.ItemManagement.Item;
+
 public class EventDirectory {
     ArrayList<Event> events;
 
     public EventDirectory() {
         events = new ArrayList<Event>();
+    }
+
+    public Event newEvent(Item item, int quantity, int difficulty, String information){
+        Event event = new Event(item, quantity, information);
+        events.add(event);
+        return event;
+    }
+
+    public Event pickRandomEvent(){
+        if(events.size() == 0){
+            return null;
+        }
+        int random = (int) (Math.random() * events.size());
+        return events.get(random);
     }
 }
